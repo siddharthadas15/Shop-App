96973025c7c5081a2f6b85baa29ba21f231be71f
siddharthadas15 <sd78268@gmail.com>
2021-05-31 01:20:27 +0530

    model setup

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const mongoose=require('mongoose');
+const Product=require('./models/product');
 
-
-mongoose.connect('mongodb://localhost:27017/ShopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MONGO CONNECTION OPEN!!!")
     })
