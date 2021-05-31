2c4cb6b0474ab62cadd8bcb8321b243276fee5c7
siddharthadas15 <sd78268@gmail.com>
2021-06-01 04:01:48 +0530

    update route done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const mongoose=require('mongoose');
+const methodOverride=require('method-override');
 const Product=require('./models/product');
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -15,6 +16,11 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+
+const categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/dogs',(req,res)=>{
 res.send('hi');
@@ -29,6 +35,50 @@ res.render('./products/index',{products});
     {
         console.log(e);
     }
+});
+
+
+// app.get('/prouduct/new',async(req,res)=>{
+// // res.render('./products/new');
+// res.send('hi');
+// });
+app.get('/products/new',(req,res)=>{
+    res.render('./products/new',{categories});
+});
+
+
+
+app.get('/products/:id', async(req,res)=>{
+try{
+    const {id}=req.params;
+    const product= await Product.findById(id);
+   res.render('./products/show',{product,categories:categories});
+}
+catch(e)
+{
+    console.log(e);
+}
+});
+
+app.get('/products/edit/:id',async(req,res)=>{
+    const {id}=req.params;
+const product= await Product.findById(id);
+res.render('products/edit',{product,categories});
+});
+
+
+app.put('/products/:id',async(req,res)=>{
+    const {id}=req.params;
+    const product=await Product.findByIdAndUpdate(id,req.body,{runValidators:true,new:true});
+    res.redirect(`/products/${product._id}`);
+});
+
+app.post('/products',async(req,res)=>{
+    
+const product = new Product(req.body);
+await product.save();
+console.log(product);
+res.redirect(`/products/${product._id}`);
 });
 
 app.listen('3000',()=>{
